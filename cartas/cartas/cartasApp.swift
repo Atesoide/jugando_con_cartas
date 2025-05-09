8d208469930b2b58aeba8e810ee70ac74e1881ae
alumno <alumno@pc-rolmos.uacj.mx>
2025-05-09 10:22:30 -0600

    [INICIALIZA] el proyecto

---
+++ cartas/cartas/cartasApp.swift
@@ -0,0 +1,17 @@
+//
+//  cartasApp.swift
+//  cartas
+//
+//  Created by alumno on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct cartasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
