600bed9c8c103a9e6ef86ced6d6febd946fead98
alumno <alumno@pc-rolmos.uacj.mx>
2025-05-09 10:45:12 -0600

    [AGREGA] modelo de mazo (incompleto)

---
+++ cartas/cartas/Modelos/Mazo_Completo.swift
@@ -0,0 +1,16 @@
+//
+//  Mazo_Completo.swift
+//  cartas
+//
+//  Created by alumno on 5/9/25.
+//
+
+import Foundation
+
+struct MazoCompleto: Identifiable, Codable{
+   //ya veremos
+    let success: Bool
+    let deck_id: String
+    let shuffled: Bool
+    let remaining: Int
+}
---
+++ cartas/cartas/servicios_api/cartas_api.swift
@@ -0,0 +1,12 @@
+//
+//  cartas_api.swift
+//  cartas
+//
+//  Created by alumno on 5/9/25.
+//
+
+import SwiftUI
+
+class CartasAPI: Codable{
+    let mazoDePrueba = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+}
